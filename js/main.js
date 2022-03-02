70e3c817993ca1e8a42ec331a9758365ae1c6254
aleballabio <aleballabio05@gmail.com>
2022-03-02 19:41:53 +0100

    done with the exercise, forgot to commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// variables
+
+const difficulty = document.querySelector("#difficulty");
+
+const grid = document.querySelector(".grid")
+grid.style.display = "none";
+
+
+
+//Button
+let buttonPlay = document.querySelector("#play");
+buttonPlay.addEventListener("click", play);
+
+function play() {
+    grid.style.display = "flex";
+    let difficultyValue = difficulty.value;
+
+    switch (difficultyValue) {
+        case "Facile":
+            for (let times = 1; times <= 49; times++) {
+                const ElementCell = document.createElement("div");
+                ElementCell.classList.add("cell");
+                ElementCell.classList.add("small");
+                ElementCell.addEventListener("click", function () {
+                    ElementCell.classList.add("change-color");
+                });
+
+                grid.append(ElementCell);
+
+                ElementCell.innerHTML += times;
+            }
+            break;
+
+        case "Medio":
+            for (let times = 1; times <= 81; times++) {
+                const ElementCell = document.createElement("div");
+                ElementCell.classList.add("cell");
+                ElementCell.classList.add("medium");
+
+                ElementCell.style.flexBasis = "calc(100% / 9)";
+                ElementCell.style.height = "calc(100% / 9)";
+                ElementCell.addEventListener("click", function () {
+                    ElementCell.classList.add("change-color");
+                });
+
+                grid.append(ElementCell);
+
+                ElementCell.innerHTML += times;
+            }
+
+            break;
+
+        case "Difficile":
+            for (let times = 1; times <= 100; times++) {
+                const ElementCell = document.createElement("div");
+
+                ElementCell.classList.add("cell");
+                ElementCell.classList.add("big");
+                ElementCell.addEventListener("click", function () {
+                    ElementCell.classList.add("change-color");
+                });
+
+                grid.append(ElementCell);
+
+                ElementCell.innerHTML += times;
+            }
+
+            break;
+    }
+}
